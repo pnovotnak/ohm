41317b025736e8353efea1385e0fa720d7377a7b
Peter Novotnak <peter.novotnak@reddit.com>
2022-06-30 16:34:01 -0700

    fix logging

--- src/nextdns/api.go
+++ src/nextdns/api.go
@@ -80,7 +80,7 @@ func StreamLogs(logC chan types.LogData) error {
 		logData := types.LogData{}
 		err = json.Unmarshal(data, &logData)
 		if err != nil {
-			log.Printf("unable to decode data: %s\n", data)
+			log.Printf("unable to unmarshal data: '%s\n' (%s)", data, err)
 			continue
 		}
 
